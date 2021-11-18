2a9dff6171b94abf86037035e24c28d0a36ae94c
Jeffrey Chan <jachan.santiago@gmail.com>
2021-11-18 02:17:22 -0400

    small optimization loading image

--- trackee/model.py
+++ trackee/model.py
@@ -1,9 +1,13 @@
 from trackee.utils import get_video, new_body
+import cv2
+
 
 class Model():
     
     def __init__(self, video, video_data=None):
         self.video = get_video(video, video_data)
+        self.capture = cv2.VideoCapture(video)
+        self.capture_frame = -1
         
     @property
     def video_name(self):
@@ -13,7 +17,14 @@ class Model():
         self.video.save(filename)
         
     def get_image(self, frame_number, bbox=True, idtext=True):
-        return self.video[frame_number]._image(bbox=bbox, idtext=idtext, min_parts=-1)
+        if frame_number - self.capture_frame != 1:
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+            
+        ret, image = self.capture.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.capture_frame = frame_number
+
+        return self.video[frame_number].draw_frame_image(image, bbox=bbox, idtext=idtext, min_parts=-1)
     
     def add_annotation(self, frame_id, body_id, coords):
         for body in self.video[frame_id]:
